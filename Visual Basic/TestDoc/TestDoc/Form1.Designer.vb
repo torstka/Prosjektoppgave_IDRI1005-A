f41669ba2383dfd69cefc5c0f9cf342d31569578
reginebk <regine_bk@hotmail.com>
2017-03-08 10:47:10 +0100

    TEEEST

--- Visual Basic/TestDoc/TestDoc/Form1.Designer.vb
+++ Visual Basic/TestDoc/TestDoc/Form1.Designer.vb
@@ -42,87 +42,87 @@ Partial Class Form1
         'TextBox1
         '
         Me.TextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None
-        Me.TextBox1.Location = New System.Drawing.Point(218, 59)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.TextBox1.Location = New System.Drawing.Point(178, 49)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(180, 29)
+        Me.TextBox1.Size = New System.Drawing.Size(148, 26)
         Me.TextBox1.TabIndex = 0
         Me.TextBox1.Text = "mysql.stud.iie.ntnu.no"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(218, 131)
-        Me.TextBox2.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.TextBox2.Location = New System.Drawing.Point(178, 109)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(180, 29)
+        Me.TextBox2.Size = New System.Drawing.Size(148, 26)
         Me.TextBox2.TabIndex = 1
         Me.TextBox2.Text = "g_oops_03"
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(218, 199)
-        Me.TextBox3.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.TextBox3.Location = New System.Drawing.Point(178, 166)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(180, 29)
+        Me.TextBox3.Size = New System.Drawing.Size(148, 26)
         Me.TextBox3.TabIndex = 2
         Me.TextBox3.Text = "mczmmM3N"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(112, 286)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button1.Location = New System.Drawing.Point(92, 238)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(137, 42)
+        Me.Button1.Size = New System.Drawing.Size(112, 35)
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "Login"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(260, 286)
-        Me.Button2.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button2.Location = New System.Drawing.Point(213, 238)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(137, 42)
+        Me.Button2.Size = New System.Drawing.Size(112, 35)
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "Cancel"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(501, 526)
-        Me.Button3.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button3.Location = New System.Drawing.Point(410, 438)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(137, 42)
+        Me.Button3.Size = New System.Drawing.Size(112, 35)
         Me.Button3.TabIndex = 5
         Me.Button3.Text = "Legg inn"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(650, 442)
-        Me.Button4.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button4.Location = New System.Drawing.Point(530, 438)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(137, 42)
+        Me.Button4.Size = New System.Drawing.Size(112, 35)
         Me.Button4.TabIndex = 6
         Me.Button4.Text = "Hent ut"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(805, 526)
-        Me.Button5.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button5.Location = New System.Drawing.Point(659, 438)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(137, 42)
+        Me.Button5.Size = New System.Drawing.Size(112, 35)
         Me.Button5.TabIndex = 7
         Me.Button5.Text = "Slett"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(121, 406)
-        Me.Button6.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button6.Location = New System.Drawing.Point(99, 338)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(281, 42)
+        Me.Button6.Size = New System.Drawing.Size(230, 35)
         Me.Button6.TabIndex = 8
         Me.Button6.Text = "Opprett Person-tabellen"
         Me.Button6.UseVisualStyleBackColor = True
@@ -130,19 +130,19 @@ Partial Class Form1
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 24
-        Me.ListBox1.Location = New System.Drawing.Point(501, 59)
-        Me.ListBox1.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(410, 49)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(440, 340)
+        Me.ListBox1.Size = New System.Drawing.Size(361, 284)
         Me.ListBox1.TabIndex = 9
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(121, 460)
-        Me.Button7.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button7.Location = New System.Drawing.Point(99, 383)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(281, 42)
+        Me.Button7.Size = New System.Drawing.Size(230, 35)
         Me.Button7.TabIndex = 10
         Me.Button7.Text = "Slett Person-Tabellen"
         Me.Button7.UseVisualStyleBackColor = True
@@ -150,48 +150,48 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(70, 65)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Location = New System.Drawing.Point(57, 54)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(70, 25)
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
         Me.Label1.TabIndex = 11
         Me.Label1.Text = "Server"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(70, 131)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Location = New System.Drawing.Point(57, 109)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(112, 25)
+        Me.Label2.Size = New System.Drawing.Size(90, 20)
         Me.Label2.TabIndex = 12
         Me.Label2.Text = "Brukernavn"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(70, 205)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.Location = New System.Drawing.Point(57, 171)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(84, 25)
+        Me.Label3.Size = New System.Drawing.Size(67, 20)
         Me.Label3.TabIndex = 13
         Me.Label3.Text = "Passord"
         '
         'Button8
         '
-        Me.Button8.Location = New System.Drawing.Point(139, 340)
-        Me.Button8.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Button8.Location = New System.Drawing.Point(114, 283)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(227, 42)
+        Me.Button8.Size = New System.Drawing.Size(186, 35)
         Me.Button8.TabIndex = 14
         Me.Button8.Text = "Registrer ny bruker"
         Me.Button8.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1049, 637)
+        Me.ClientSize = New System.Drawing.Size(858, 531)
         Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -207,7 +207,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
-        Me.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
