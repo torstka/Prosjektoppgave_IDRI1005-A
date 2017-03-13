e814192cb5bc923d8502dc7a53da82821d56cccc
ninaind <nind_82@hotmail.com>
2017-03-13 11:36:21 +0100

    enda en kanpp

--- Visual Basic/TestDoc/TestDoc/DataMyBlood.Designer.vb
+++ Visual Basic/TestDoc/TestDoc/DataMyBlood.Designer.vb
@@ -25,12 +25,14 @@ Partial Class DataMyBlood
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Imprint MT Shadow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(105, 258)
+        Me.Button1.Location = New System.Drawing.Point(33, 273)
         Me.Button1.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(288, 191)
@@ -40,7 +42,7 @@ Partial Class DataMyBlood
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(476, 258)
+        Me.Button2.Location = New System.Drawing.Point(401, 273)
         Me.Button2.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(306, 191)
@@ -50,18 +52,39 @@ Partial Class DataMyBlood
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(851, 258)
+        Me.Button3.Location = New System.Drawing.Point(779, 273)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(312, 191)
         Me.Button3.TabIndex = 2
         Me.Button3.Text = "Spørre skjema"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Imprint MT Shadow", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(497, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(139, 33)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Historikk"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(940, 25)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(151, 44)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Logg ut"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'DataMyBlood
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 28.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1912, 777)
+        Me.ClientSize = New System.Drawing.Size(1165, 777)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -70,10 +93,13 @@ Partial Class DataMyBlood
         Me.Name = "DataMyBlood"
         Me.Text = "DataMyBlood"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
 End Class
