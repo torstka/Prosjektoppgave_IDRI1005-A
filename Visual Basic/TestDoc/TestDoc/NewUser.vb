5dcb878116a0a0ed717f31882be60d4a7dd71a84
Torstein Kårstad <torsteinkaarstad@yahoo.no>
2017-03-15 10:27:54 +0100

    new user blabla

--- Visual Basic/TestDoc/TestDoc/NewUser.vb
+++ Visual Basic/TestDoc/TestDoc/NewUser.vb
@@ -14,8 +14,8 @@ Public Class NewUser
         Dim Etternavn = txtLastname.Text
         Dim Personnummer = txtPersonnr.Text
         Dim Adresse = txtAdress.Text
-        Dim Postnummer = txtPostnr
-        Dim Telefon = txtPostnr.Text
+        Dim Postnummer = txtPostnr.Text
+        Dim Telefon = txtPhone.Text
         Dim Epost = txtEmail.Text
         Dim Passord = txtPassword.Text
 
